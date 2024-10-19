377aa00efb9e00a5d7a6bfddd74788c7d88e6d6d
MaSh-1009 <mash1009nico@gmail.com>
2024-10-19 16:51:30 +0900

    Update main.ino
    バグ修正版マージ

--- source/arduino/commander/main.ino
+++ source/arduino/commander/main.ino
@@ -1,7 +1,8 @@
 #include <Ethernet2.h>
+// #include "commander.h"
+#include "parse.h"
 
-const int RECV_CMD_FIELD_NUM_MAX = 10;
-String recvCmdFieldList[RECV_CMD_FIELD_NUM_MAX];
+#define MAX_BUF_LEN 8
 
 byte mac[] = {
   0x90, 0xA2, 0xDA, 0x10, 0xE8, 0x7F
@@ -10,58 +11,49 @@ IPAddress ip(192, 168, 5, 10);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress gateway(192, 168, 5, 10);
 EthernetServer socketServer(55555);
-String recvMsg = "";
 
-void setup()
-{
-  // initialize the ethernet device
+uint8_t buf[MAX_BUF_LEN];
+int32_t ibuf = 0;
+
+void buf_init();
+
+void setup() {
   Ethernet.begin(mac, ip, gateway, subnet);
-  // start listening for clients
   socketServer.begin();
-  // Open serial communications for debug
   Serial.begin(9600);
 }
 
-void loop()
-{
-  // Return of EthernetClient.available(): 
-  // Client object. If the client has no readable data,
-  // this object evaluates to false in the if statement.
+void loop() {
+  struct cmd_packet res;
   EthernetClient client = socketServer.available();
 
-  if (client)
-  {
-    // Receive message
-    char recvByte = client.read();
-    if ((int)recvByte > 0)
-    {
-      recvMsg = recvMsg + String(recvByte);
-      if (recvByte == '\r')
-      {
-        Serial.println("Received message: " + recvMsg);
+  if (client) {
+    buf_init();
 
-        // Parse message
-        recvMsg.trim();
-        int fieldNum = split(recvMsg, ' ', recvCmdFieldList, RECV_CMD_FIELD_NUM_MAX);
-        recvMsg = "";
-        if (fieldNum < 2) return;
-        for(int i = 0; i < RECV_CMD_FIELD_NUM_MAX; i++)
-        {
-          if (recvCmdFieldList[i] != "") Serial.println("Recv: " + recvCmdFieldList[i]);
+    while (client.connected()) {
+      if(client.available()) {
+        buf[ibuf++] = client.read();
+
+        if (ibuf >= 8) {
+          parse(buf, ibuf, &res);
+          ibuf = 0;
         }
 
-        // Execute command
-        for (int i = 0; i < RECV_CMD_NUM; i++)
-        {
-          if (recvCmdList[i].cmdName == recvCmdFieldList[1])
-            int res = (int)(recvCmdList[i].cmdFunc)(recvCmdFieldList[2]);
+        if (!(0 <= res.cmd && res.cmd < CMD_MAX)) {
+          //Serial.println("Error: Invalid Cmd No.: %d !\n", res.cmd);
+        } else {
+          struct cmd_func_list cmd = recv_cmd_list[res.cmd];
+          //Serial.println("Recv Command: %s, opt = %x\n", cmd.cmd_name, res.opt);
+          cmd.cmd_func(res.opt);
         }
       }
     }
-    else
-    {
-      Serial.println("No message");
-      recvMsg = "";
-    }
+
+    client.stop();
   }
+}
+
+void buf_init() {
+  memset(buf, 0, sizeof buf);
+  ibuf = 0;
 }
